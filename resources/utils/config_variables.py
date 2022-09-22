640290056d42f2a64ca35a3337c2d2c43ef16bbf
Rafael Fernandes da Silva <81975862+Rafael-Orbia@users.noreply.github.com>
2022-09-22 11:23:18 -0300

    update

--- resources/utils/config_variables.py
+++ resources/utils/config_variables.py
@@ -7,27 +7,25 @@ ENVIRONMENT = "UAT"
 DB_API_MODULE_NAME = "pymssql"
 
 UAT_ENVIRONMENT = {
-    'TESTED_HOST':"https://uat.satelital.com.br",
-    'DB_NAME':"dbPRW",
-    'DB_USER':"iusr_orbia_prw",
-    'DB_PASSWORD':"orbia@2020",
-    'DB_HOST':"sqluat01.satelital.com.br",
-    'DB_PORT':"1433",
-    'USER_PASSWORD':"Unica@123",
-    'ENCRIPTED_DEFAULT_PASSWORD': "00610485987E98E82C2D831F6C2FF155",
+    'TESTED_HOST':"",
+    'DB_NAME':"",
+    'DB_USER':"",
+    'DB_PASSWORD':"",
+    'DB_HOST':"",
+    'DB_PORT':"",
+    'USER_PASSWORD':"",
+    'ENCRIPTED_DEFAULT_PASSWORD': "",
     'SQL_FOLDER': "UAT"
 }
 
 RC_ENVIRONMENT = {
-    'TESTED_HOST':"https://rc.satelital.com.br",
-    'DB_NAME':"dbPRW",
-    'DB_USER':"usr_orbia",
-    'DB_PASSWORD':"orbia@123",
-    'DB_HOST':"sqlrc01.satelital.com.br",
-    'DB_PORT':"1433",
-    'USER_PASSWORD':"Test123!",
-    'ENCRIPTED_DEFAULT_PASSWORD': "2168AD5E463D9ACCB215EDAAFA31C8D9",
-    "SQL_FOLDER": "RC"
+    'TESTED_HOST':"",
+    'DB_NAME':"",
+    'DB_USER':"",
+    'DB_PASSWORD':"",
+    'DB_HOST':"",
+    'DB_PORT':"",
+    'USER_PASSWORD':"",
+    'ENCRIPTED_DEFAULT_PASSWORD': "",
+    "SQL_FOLDER": ""
 }
-
-CARTAO_CREDITO_TEST = "4481530710186111"
